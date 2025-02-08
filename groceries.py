c76c08bd94558b6ac5d08474da155f64badc525d
Sergio <sergiobaksh@Sergios-MacBook-Pro.local>
2025-02-08 18:12:22 -0500

    first commit

---
+++ groceries.py
@@ -0,0 +1,10 @@
+# eggs: $2, banana: $1, salad: $10, steak & rice: $30
+
+print("breakfast cost:")
+print(2 + 1)
+
+print("lunch cost:")
+print(10)
+
+print("dinner cost:")
+print(30 + 2)
---
+++ imcOne.py
@@ -0,0 +1,5 @@
+print(3 + 10)
+
+print("i'm the best")
+
+print(7)
